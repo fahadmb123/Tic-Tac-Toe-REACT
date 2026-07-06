4f90a97d04209e3aca14da7a7bdaa5a43721c163
Fahad MB <fahadmbasheer@gmail.com>
2026-07-06 22:25:10 +0530

    Winner Function added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,29 @@ function Square (props) {
   return  <button className="square" onClick={props.onClick}>{props.value}</button>
 }
 
+
+function Winner (values) {
+  const coordinates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  
+  for (let i=0;i<coordinates.length;i++) {
+    const [a,b,c] = coordinates[i]
+    if (values[a] && values[b] === values[a] && values[b] === values[c]) {
+      return values[c]
+    }
+  }
+  return null
+}
+
+
 function App() {
 
   const [status,setStatus] = useState(true)
