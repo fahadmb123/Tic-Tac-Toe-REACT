634bcf6f9a9e3b69d5f060c7a00631b9951e576a
Fahad MB <fahadmbasheer@gmail.com>
2026-07-06 20:33:04 +0530

    Full structure Body Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
 
+
 function App() {
 
   return (
     <>
-      <div className='mainDiv'>
+      <div className='game'>
+        <h1 className="title">Tic-Tac-Toe</h1>
+        <h2 className="second-title">Wannna Playy...</h2><br />
 
+
+        <div className="board">
+
+
+          <button className="square"></button>
+          <button className="square"></button>
+          <button className="square"></button>
+
+          <button className="square"></button>
+          <button className="square"></button>
+          <button className="square"></button>
+
+          <button className="square"></button>
+          <button className="square"></button>
+          <button className="square"></button>
+
+
+        </div>
       </div>
     </>
   )
